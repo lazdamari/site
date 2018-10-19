adee9290d92aaba28c51d9710f42e5578484e080
Ensar Günaydın <bote.ensargunaydin@gmail.com>
2018-10-19 15:45:26 +0300

    .

---
+++ application/views/blank_v..php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--[if IE 9]>
+<html lang="tr" class="ie9"> <![endif]-->
+<!--[if gt IE 9]>
+<html lang="tr" class="ie"> <![endif]-->
+<!--[if !IE]><!-->
+<html dir="ltr" lang="tr">
+
+<head>
+
+    <?php echo base_url("includes/head"); ?>
+
+</head>
+
+<body class="front-page transparent-header">
+
+<?php echo base_url("includes/include_script"); ?>
+
+</body>
+
+
+</html>
